0c2aa5045049af017ae7691980c07631426246a0
a-oboh <albertoboh7@gmail.com>
2023-06-09 14:49:11 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.relight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:relight/app/app.dart';
+import 'package:relight/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
